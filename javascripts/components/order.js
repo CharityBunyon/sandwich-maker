2bc56d011f3efb4ceb4957cdbe33eceee1dbe8df
Charity Bunyon <cseymou5@gmail.com>
2019-09-26 12:41:03 -0500

    printed breads to dom

---
+++ javascripts/components/order.js
@@ -0,0 +1,34 @@
+import bread from './bread.js';
+import utilities from '../helpers/utilities.js';
+
+const createFinalOrder = (items) => {
+    let domString2 = '';
+    for (let i = 0; i < items.length; i++){
+        domString2 += `<h2>${items[i].name} $${items[i].price}</h2>`;
+    }
+    utilities.printToDom('final-order', domString2);
+    //this will overwrite the button but we could make another div
+}
+
+const createOrderEvent = () => {
+    const selectedBreads = bread.getSelectedBreads();
+    // const selectedMeats = meat.getSelectedMeats(); TODO
+    createFinalOrder(selectedBreads);
+    //12- created a final function
+
+};
+//10 selected cheeses will be in cheese.js
+
+const printOrderButton = () => { 
+    const domString = `<button id="order-button" class="btn btn-primary">Make Sandwich</button>`
+    utilities.printToDom('final-order', domString);
+    document.getElementById('order-button').addEventListener('click', createOrderEvent);
+    // 9-added event, put an id on the button, created a function
+
+};
+
+//8- made function and added import
+
+
+
+export default { printOrderButton };
