50ff0f624900b52d0f6aeb29df94a1494450229e
Charity Bunyon <cseymou5@gmail.com>
2019-09-26 14:40:27 -0500

    added all modules to print off to the dom

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,18 @@
 import bread from './components/bread.js';
 import order from './components/order.js';
+import meat from './components/meat.js';
+import cheese from './components/cheese.js';
+import condiment from './components/condiment.js';
+import veggie from './components/veggie.js';
 // 3rd thing: Make init function in the main.js
 
 const init = () => {
     //do stuff
     bread.printBreadOptions();
+    meat.printMeatOptions();
+    cheese.printCheeseOptions();
+    condiment.printCondimentOptions();
+    veggie.printVeggieOptions();
     //7- Making the order btn that will print out the items I check off. I also imported above and made this file in the components folder
     order.printOrderButton();
     //inside store theres a function call this and run it.
