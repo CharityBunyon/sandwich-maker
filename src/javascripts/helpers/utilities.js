1e1c011f8383e21a9c1c592797a7cb748ecbe286
Charity Bunyon <cseymou5@gmail.com>
2019-12-21 22:21:30 -0600

    added webpack

---
+++ src/javascripts/helpers/utilities.js
@@ -0,0 +1,7 @@
+const printToDom = (divId, toPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = toPrint;
+};
+// first thing
+export default { printToDom };
+// second thing is to export this to let other files no about it
