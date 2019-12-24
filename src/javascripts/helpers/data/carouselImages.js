e65531623445d37269db1842673cb4da09cb4fa7
Charity Bunyon <cseymou5@gmail.com>
2019-12-24 10:05:17 -0600

    added images for deployment

---
+++ src/javascripts/helpers/data/carouselImages.js
@@ -0,0 +1,19 @@
+const images = [
+  {
+    name: 'image1',
+    img: 'https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    name: 'image2',
+    img: 'https://images.unsplash.com/photo-1550507992-eb63ffee0847?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    name: 'image3',
+    img: 'https://images.unsplash.com/photo-1558985250-27a406d64cb3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+  },
+];
+
+
+const getImages = () => images;
+
+export default { getImages };
